eefdc755d58de93866bd4fd7cb8eece668699b32
Sujit Khandagale <sujitkh94@outlook.com>
2023-06-07 15:03:54 +0530

    default route change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Layouts><Home /></Layouts>} />
+        <Route path='/' element={<Layouts><Products /></Layouts>} />
         <Route path='/signin' element={<Layouts><SignIn /></Layouts>} />
         <Route path='/signup' element={<Layouts><SignUp/></Layouts>} />
         <Route path='/products' element={<Layouts><Products /></Layouts>} />
